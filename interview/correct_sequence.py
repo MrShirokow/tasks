1c06a8b4d6c37049bb31d437b5cbd1b7d76e89b7
MrShirokow <mr.shirokow@gmail.com>
2022-11-11 03:52:38 +0500

    refactoring correct sequence task

--- interview/correct_sequence.py
+++ interview/correct_sequence.py
@@ -1,3 +1,13 @@
+"""
+Дана последовательность из скобок ( и ). 
+Нужно написать функцию, которая проверяет корретность такой последовательности
+
+()()() - True
+()(()) - True
+())( - False
+"""
+
+
 def check_sequence(sequence: str) -> bool:
     validator = 0
     operations = {"(": 1, ")": -1}
@@ -5,12 +15,16 @@ def check_sequence(sequence: str) -> bool:
         validator += operations[bracket]
         if validator < 0:
             return False
-    return True if validator == 0 else False
+    return validator == 0
 
 
 def main():
-    sequence = input()
-    print(check_sequence(sequence))
+    assert check_sequence('()()()')
+    assert check_sequence('(())()()')
+    assert not check_sequence('((())))')
+    assert not check_sequence(')(')
+    assert not check_sequence('(')
+    print('\nAll tests passed!\n')
 
 
 if __name__ == '__main__':
