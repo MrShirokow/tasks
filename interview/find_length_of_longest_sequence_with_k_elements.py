68e5f585e44c0f548074f04b4e9adf8d0d9bafa5
Serj <mr.shirokow@gmail.com>
2023-01-16 00:08:05 +0500

    UPDATE: last fix of task (find length of longest subsequence with different K elements)

---
+++ interview/find_length_of_longest_sequence_with_k_elements.py
@@ -0,0 +1,37 @@
+from typing import Iterable
+from collections import OrderedDict
+
+
+def find_length_of_longest_sequence(chars: Iterable, k: int) -> int:
+    right_indexes = {}
+    left_index = -1
+    max_length = count = 0
+
+    for index, char in enumerate(chars):
+        if char not in right_indexes:
+            count += 1
+        right_indexes[char] = index
+        if count <= k:
+            max_length = max(max_length, index - left_index)
+        else:
+            excess_char = min(right_indexes, key=right_indexes.get)
+            left_index = right_indexes[excess_char]
+            right_indexes.pop(excess_char)
+            count -= 1
+    return max_length
+
+
+assert find_length_of_longest_sequence('', 3) == 0
+assert find_length_of_longest_sequence('abbabbcccbbcb', 2) == 9
+assert find_length_of_longest_sequence('abbabbcccbbcb', 4) == 13
+assert find_length_of_longest_sequence('aaaaaaaaaaaaaaaaaaa', 2) == 19
+assert find_length_of_longest_sequence('baaaaaaaaaaaaaaaaaaab', 2) == 21
+assert find_length_of_longest_sequence('cbbcbbdbc', 3) == 9
+assert find_length_of_longest_sequence('ababcacdaddbbdb', 2) == 6
+assert find_length_of_longest_sequence('abcdebcdecdedeea', 4) == 14
+assert find_length_of_longest_sequence('aaaabbbbbccccccdddddddaaaaaaaaa', 1) == 9
+assert find_length_of_longest_sequence('aaaabbbbbccccccdddddddaaaaaaaaa', 2) == 16
+assert find_length_of_longest_sequence('abbcbbdbc', 3) == 8
+assert find_length_of_longest_sequence('ababcacdaddbbdb', 3) == 8
+assert find_length_of_longest_sequence('cabcaccbbddbcbcdbcbdbcdd', 3) == 19
+assert find_length_of_longest_sequence('cabcaccbbaddabbabeabeabeabbbeeabcd', 3) == 20
